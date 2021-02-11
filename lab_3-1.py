71347e72eac70c277cc7bee73893d13a7d821b04
Patrick Tiernan <p21ptiernan@fairfieldprep.org>
2021-02-11 13:13:34 -0500

    submit cycle 16 day 3 labs

---
+++ lab_3-1.py
@@ -0,0 +1,6 @@
+# Author PT 2/11/21
+
+with open('my_file.txt', 'w') as ofile:
+    ofile.write("PT 2/11/21\n")
+    ofile.write("Hello World!\n")
+    ofile.write("Chillin\n")
---
+++ lab_3-2.py
@@ -0,0 +1,5 @@
+# Author PT 2/11/21
+
+with open("my_file.txt", "a") as afile:
+    afile.write("Facetime\n")
+    afile.write("Hangout with all my friends\n")
